9568e3567462c82ed26b7d14b1da5809443b719c
Michael Napoleon <michael.napoleon@gmail.com>
2013-10-23 07:49:25 -0400

    added shell for assignment #5

---
+++ Lab5/Stats101-Assignment5.R
@@ -0,0 +1,50 @@
+### Assignment #5
+
+### Question 1
+### Run a regression model with salary as the outcome variable and years of experience as the predictor variable. 
+### What is the 95% confidence interval for the regression coefficient? Type your answer exactly as it appears 
+### in R but include only two decimal places (for example, if the 95% confidence interval is -1 to +1 then type -1.00 1.00)
+
+
+### Question 2
+### Run a regression model with salary as the outcome variable and courses as the predictor variable.
+### What is the 95% confidence interval for the regression coefficient?
+
+
+### Question 3
+### Run a multiple regression model with both predictors and compare it with both the model from Question 1 
+### and the model from Question 2. Is the model with both predictors significantly better than:
+
+
+### Question 4
+### Run a standardized multiple regression model with both predictors. Do the confidence interval values 
+### differ from the corresponding unstandardized model?
+
+
+### Question 5
+### What function could you use to take a random subset of the data?
+
+
+### Question 6
+### Run the following command in R: set.seed(1). Now take a random subset of the original data so 
+### that N=15. Is the correlation coefficient between salary and years of experience in this sample higher or 
+### lower than in the whole data set?
+
+
+### Question 7
+### Take a subset of the original data from row 51 to 70. What is the percentage of variance explained by 
+### a multiple regression model with both predictors (Provide your result with no decimal place)
+
+
+### Question 8
+### Using model comparison, which model provides the best fit for the subsetted data from Question 7?
+
+
+### Question 9
+### What is the correlation between the salary values predicted by the multiple regression model 
+### and the actual salary scores in the subsetted data? (Provide your result rounded to 2 decimal places)
+
+
+### Question 10
+### Compute the correlation between the scores predicted by the multiple regression model 
+### and the residuals from the same model. Is the correlation statistically significant?
