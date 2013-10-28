7936e4c30f37110859b93a0d7d6c1e10ddc92fe6
Michael Napoleon <michael.napoleon@gmail.com>
2013-10-28 13:47:30 -0400

    Added shell for assignment #6

---
+++ Lab6/Stats101-Assignment6.R
@@ -0,0 +1,48 @@
+### Assignment #6
+
+
+### Question 1
+## In a model predicting salary, what is the unstandardized regression coefficient for 
+### years, assuming years is the only predictor variable in the model?
+
+
+### Question 2
+### In a model predicting salary, what is the 95% confidence interval for the unstandardized
+### regression coefficient for years, assuming years is the only predictor variable in the model?
+
+
+### Question 3
+### In a model predicting salary, what is the unstandardized regression coefficient for years, 
+### assuming years and courses are both included as predictor variables in the model?
+
+
+### Question 4
+### In a model predicting salary, what is the 95% confidence interval for the unstandardized 
+### regression coefficient for years, assuming years and courses are both included as predictor variables 
+### in the model?
+
+
+### Question 5
+### What is the predicted difference in salary between Doctors and Lawyers assuming an equal and average 
+### number of years and courses?
+
+
+### Question 6
+### Is the predicted difference between Doctors and Lawyers statistically significant?
+
+
+### Question 7
+### What is the predicted difference in salary between Doctors and Teachers assuming an 
+### equal and average number of years and courses?
+
+
+## Question 8
+### Is the predicted difference between Doctors and Teachers statistically significant?
+
+
+### Question 9
+### What is the actual difference in mean salary between Doctors and Teachers?
+
+
+### Question 10
+### What combination of predictors represents the best model in terms of predicting salary?
