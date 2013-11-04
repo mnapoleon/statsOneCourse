c362a097ba1b6536735c82d5a778ea3f5ebd172f
Michael Napoleon <michael.napoleon@gmail.com>
2013-11-04 09:48:50 -0500

    added shell for assignment #7 questions

---
+++ Lab7/Stats101-Assignment7.R
@@ -0,0 +1,44 @@
+### Assignment # 7
+
+
+### Question 1
+### What is the correlation between extraversion and happiness?
+
+
+### Question 2
+### What is the correlation between extraversion and diversity of life experience?
+
+
+### Question 3
+### What is the correlation between diversity of life experience and happiness?
+
+
+### Question 4
+### What percentage of variance in happiness is explained by extraversion?
+
+### Question 5
+### What percentage of variance in happiness is explained by a model with both 
+### extraversion and diversity of life experience as predictors?
+
+
+### Question 6
+### What is the 95% confidence interval for the regression coefficient for extraversion 
+### when it is the only predictor of happiness?
+
+
+### Question 7
+### What is the 95% confidence interval for the regression coefficient for extraversion 
+### when it and diversity of life experience are both predictors of happiness?
+
+
+### Question 8
+### What is the unstandardized regression estimate of the indirect effect?
+
+
+### Question 9
+### What is the z-value of the Sobel test?
+
+
+### Question 10
+### Do these analyses suggest full mediation, partial mediation, or no mediation?
+
