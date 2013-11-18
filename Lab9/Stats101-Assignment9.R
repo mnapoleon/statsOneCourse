571d3fd14191efd2813aea7a60c6b329b8f6a1f4
Michael Napoleon <michael.napoleon@gmail.com>
2013-11-18 08:26:45 -0500

    setup for assignment #9

---
+++ Lab9/Stats101-Assignment9.R
@@ -0,0 +1,46 @@
+### Assignment #9
+
+
+### Question 1
+### What is the class of Haste and Prime in R?
+
+
+### Question 2
+### After converting Haste and Prime to factors, run an ANOVA 
+### with both Haste and Prime as independent variables. Is the effect of Haste significant?
+
+
+### Question 3
+### Is the effect of Prime significant?
+
+
+### Question 4
+### Is the interaction significant?
+
+
+### Question 5
+### Save the ANOVA summary in a table and run 
+### Tukey's pairwise comparison on all group means. 
+### Do each level of Haste significantly differ from one another?
+
+
+### Question 6
+### What is the partial eta-squared value for the effect of Haste? (round to 2 decimal places).
+
+
+### Question 7
+### What is the partial eta-squared value for the interaction? (round to 2 decimal places).
+
+
+### Question 8
+### Let's now run simple effects of Prime at each level of Haste. At which level of 
+### Haste is the effect of Prime significant?
+
+
+### Question 9
+### What is the partial eta-squared value for the effect of Prime when 
+### people were early? (round to 2 decimal places).
+
+
+### Question 10
+### Which one of the following statements best illustrates the main finding of the study?
