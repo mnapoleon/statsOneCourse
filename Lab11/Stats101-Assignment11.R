3e7f4cd82ce2c2ea051b296367304e0b07702b89
unknown <mnapoleo@mnapoleo8460a.vertexinc.com>
2013-12-03 14:21:17 -0500

    added shell for assigment #11

---
+++ Lab11/Stats101-Assignment11.R
@@ -0,0 +1,48 @@
+### Assignment # 11
+
+
+### Question 1
+###Using a t-test, compare verbal scores before and after training in the fixed condition. 
+### Is the difference pre-test to post-test significant?
+
+
+### Question 2
+### What are the degrees of freedom for the comparison between pre-test and post-test for the spatial scores?
+
+
+### Question 3
+### Run a Wilcoxon test for the same comparison (pre-test to post-test on spatial scores, fixed condition). 
+### Which of the two tests gives the highest p-value for the comparison?
+
+
+### Question 4
+### What is the effect size (Cohen's d) for the difference between pre-test and post-test spatial scores 
+### for the malleable condition? (round to two decimal places)
+
+
+### Question 5
+### Which of the three tasks shows the largest improvements from pre-test to post-test, in the fixed condition?
+
+
+### Question 6
+### Which of the three tasks shows the largest improvements from pre-test to post-test, in the malleable condition?
+
+
+### Question 7
+### Conduct Mann-Whitney comparisons between all tasks at pre-test. Which task(s) differ significantly 
+### from the other two in pre-test scores?
+
+
+### Question 8
+### Which feedback condition led to the largest improvements overall?
+
+
+### Question 9
+### Which task is largely driving this effect?
+
+
+### Question 10
+### Based on the present data, are you convinced that malleable feedback is beneficial to performance 
+### when engaging in a cognitive training program?
+
+
